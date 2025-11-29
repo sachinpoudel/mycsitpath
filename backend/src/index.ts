55ac6f870e3d2db01642c0fe25238e68ce024ae5
sachinpoudel <poudelsachin009@gmail.com>
2025-11-29 11:06:20 +0545

    dockerize

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,33 @@
 import express from 'express';
-import type { Request, Response } from 'express';
+import path from 'path';
 
 import cors from 'cors';
 import rootRouter from './routes/rootRoutes.js';
+import { fileURLToPath } from 'url';
 
 const app = express()
 const port = process.env.PORT || 3001;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors({
   "origin": "*"
 ,  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
   "preflightContinue": false,
   "optionsSuccessStatus": 204}));
+
 app.use(express.json());
-import bcrypt from 'bcrypt';
-const hash = await bcrypt.hash('sachin123', 10);
-console.log(hash);
 app.use("/api", rootRouter)
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/{*path}', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../public', 'index.html'));
+// });
+
+// import bcrypt from 'bcrypt';
+// const hash = await bcrypt.hash('sachin123', 10);
+// console.log(hash);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
